f849ceeb79494512513b38346d081c2fe89174c5
Kenan Kajkus <kenan@kajkus.eu>
2017-11-09 13:57:19 +0100

    Song contain songdata

---
+++ src/main/java/model/Song.java
@@ -0,0 +1,75 @@
+package model;
+
+import javafx.beans.value.ObservableValue;
+
+public class Song implements interfaces.Song {
+    @Override
+    public String getAlbum() {
+        return null;
+    }
+
+    @Override
+    public void setAlbum(String album) {
+
+    }
+
+    @Override
+    public String getInterpret() {
+        return null;
+    }
+
+    @Override
+    public void setInterpret(String interpret) {
+
+    }
+
+    @Override
+    public String getPath() {
+        return null;
+    }
+
+    @Override
+    public void setPath(String path) {
+
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public void setTitle(String title) {
+
+    }
+
+    @Override
+    public long getId() {
+        return 0;
+    }
+
+    @Override
+    public void setId(long id) {
+
+    }
+
+    @Override
+    public ObservableValue<String> pathProperty() {
+        return null;
+    }
+
+    @Override
+    public ObservableValue<String> albumProperty() {
+        return null;
+    }
+
+    @Override
+    public ObservableValue<String> interpretProperty() {
+        return null;
+    }
+
+    @Override
+    public ObservableValue<String> titleProperty() {
+        return null;
+    }
+}
