0496a2f8788a8264979aae35413e07d5b7d0794d
Kenan Kajkus <kenan@kajkus.eu>
2017-11-09 21:01:28 +0100

    all kind of views composing the media player

---
+++ src/main/java/View/SaveLoadView.java
@@ -0,0 +1,20 @@
+package View;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.HBox;
+
+/**
+ *This view part contains all elements to control saving and loading strategies
+ */
+
+public class SaveLoadView extends HBox {
+
+    ChoiceBox<String> strategyChoise = new ChoiceBox<>();
+    private Button load = new Button("Load");
+    private Button save = new Button("Save");
+
+    public SaveLoadView(){
+        getChildren().addAll(strategyChoise, load, save);
+    }
+}
