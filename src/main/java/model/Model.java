f11a5d5e9cb624ce33f93df50801c87f202c152b
Kenan Kajkus <kenan@kajkus.eu>
2017-11-15 01:09:08 +0100

    optimizes mp3 check

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -17,7 +17,9 @@ public class Model{
 
     public void setLibrary(File[] file) {
         for (int i = 0; i < file.length; i++){
-            if(file[i].toString().contains(".mp3")) {
+            String fileString = file[i].toString();
+            //checks if file is an mp3 therefore it looks up if last four chars of fileString equals ".mp3"
+            if(fileString.indexOf(".mp3",fileString.length()-5)>0) {
                 Song temp = new Song(file[i]);
                 library.add(temp);
             }
