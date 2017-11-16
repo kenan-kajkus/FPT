aafd011c8e4304ae2000c77e8ce6f4c50bd75968
Kenan Kajkus <kenan@kajkus.eu>
2017-11-16 02:33:51 +0100

    huge commit on mediaplayer now th program is capable of playing/pause songs and skip one

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,14 +2,18 @@ package controller;
 
 import interfaces.Song;
 import javafx.scene.control.ListView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import model.Model;
-import view.MusicplayerView;
 import view.View;
 import java.io.File;
 
 public class Controller{
+    Media m;
+    int currentSong = 0;
+    private MediaPlayer mp;
     private File libFolder;
     private Model model;
     private View view;
@@ -31,21 +35,53 @@ public class Controller{
         view.showLibMeta(() ->{
             showMetaData(view.getLibrary());
         });
-        //MusicplayerEventhandler ---
-        MusicplayerView tempMPView = view.getMusicPlayer();
-        tempMPView.onPlay(() -> {
-            //TODO implements function of PlayBtn ;
+        view.removeFromPlaylist(() ->{
+            model.getPlaylist().remove(view.getPlaylist().getSelectionModel().getSelectedItem());
         });
-        tempMPView.onPause(() -> {
-            //TODO implements function of PauseBtn ;
+        view.commit(() ->{
+            Song song = view.getLibrary().getSelectionModel().getSelectedItem();
+            song.setInterpret(view.getInterpretText().getText());
+            song.setAlbum(view.getAlbumText().getText());
+            song.setTitle(view.getTitleText().getText());
         });
-        tempMPView.onNextsong(() -> {
-            //TODO implements function of nextSongBtn ;
+        view.play(() ->{
+            if(m==null)
+                m  = new Media(new File(model.getPlaylist().get(0).getPath()).toURI().toString());
+            if(mp==null) {
+                mp = new MediaPlayer(m);
+                mp.setOnEndOfMedia(() -> {
+                    newmp();
+                });
+            }
+            mp.play();
         });
-        tempMPView.onCommit(() -> {
-            //TODO implements function of commitBtn ;
+
+        view.pause(() ->{
+            mp.pause();
         });
-        //---End MusicplayerEventhandler
+
+        view.next(()->{
+            mp.stop();
+            if (++currentSong >= model.getPlaylist().size())
+                currentSong = 0;
+            mp = new MediaPlayer(new Media(new File(model.getPlaylist().get(currentSong).getPath()).toURI().toString()));
+            mp.setOnEndOfMedia(() ->{
+                newmp();
+            });
+            mp.play();
+        });
+    }
+
+
+    private void newmp(){
+        if (++currentSong >= model.getPlaylist().size())
+            currentSong = 0;
+        mp = new MediaPlayer(new Media(new File(model.getPlaylist().get(currentSong).getPath()).toURI().toString()));
+        mp.play();
+        mp.setOnEndOfMedia(()->{
+            newmp();
+        });
+        System.gc();
     }
 
     private void addToPlaylist(Song s) {
@@ -63,6 +99,8 @@ public class Controller{
 
     private void showMetaData(ListView<Song> listView){
         Song tempSong = listView.getSelectionModel().getSelectedItem();
+        if(tempSong == null)
+            return;
         view.setMetaData(tempSong.getTitle(),tempSong.getInterpret(),tempSong.getAlbum());
     }
 }
--- src/main/java/view/MetaDataView.java
+++ src/main/java/view/MetaDataView.java
@@ -5,18 +5,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 class MetaDataView extends VBox {
-    private final Label album = new Label("Album:");
-    private final Label interpret = new Label("Interpret:");
-    private final Label title = new Label("Titel:");
-    private final TextField titleText = new TextField();
-    private final TextField interpretText = new TextField();
-    private final TextField albumText = new TextField();
+
     MetaDataView(){
-        getChildren().addAll(title, titleText, interpret, interpretText, album, albumText);
+
     }
     void setMetaData(String title, String interpret, String album){
-        titleText.setText(title);
-        interpretText.setText(interpret);
-        albumText.setText(album);
+
     }
 }
