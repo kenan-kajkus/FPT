9422534e6194e290aebb82036bb380ddcd72b7db
kenan-kajkus <kenan.kajkus@gmai.com>
2018-01-25 02:57:23 +0100

    sending from client to server

--- src/main/java/tcp/TCPClient.java
+++ src/main/java/tcp/TCPClient.java
@@ -1,4 +1,17 @@
 package tcp;
 
-public class TCPClient {
+import java.io.*;
+import java.net.Socket;
+
+public class TCPClient implements Runnable{
+    @Override
+    public void run() {
+        try(Socket serverCon = new Socket("localhost",5020);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(serverCon.getOutputStream()));) {
+            out.write("hello");
+            out.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/tcp/TCPServer.java
+++ src/main/java/tcp/TCPServer.java
@@ -1,4 +1,29 @@
 package tcp;
 
-public class TCPServer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer implements Runnable{
+    @Override
+    public void run() {
+        try(ServerSocket server = new ServerSocket(5020)){
+            while (true){
+                try(Socket client = server.accept();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    OutputStream out = client.getOutputStream()){
+                    String a = in.readLine();
+                    System.out.println(a);
+                }catch (IOException e2){
+                    e2.printStackTrace();
+                }
+
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
